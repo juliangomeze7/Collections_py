033541a0b27acba08b40a5c95a90310aedd7aff2
Julián Gómez <103852631+juliangomeze7@users.noreply.github.com>
2025-11-19 07:35:42 -0500

    Update 13 interseccion.py

--- 13 interseccion.py
+++ 13 interseccion.py
@@ -1,1 +1,9 @@
+def interseccion_pares(A, B):
+    pares_A = {x for x in A if x % 2 == 0}
+    pares_B = {x for x in B if x % 2 == 0}
+    return pares_A & pares_B
+
+A = [2, 4, 6, 8]
+B = [4, 5, 6, 9]
+print(interseccion_pares(A, B))
 
